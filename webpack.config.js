1995b4d7efbedd11d35267985a9b4e7e53e929df
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2016-03-03 16:59:06 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var sysPath = require("path");
+var root = __dirname;
+var srcRoot = sysPath.join(root, "src");
+
+module.exports = {
+  context: srcRoot,
+
+  entry: "index.js",
+
+  output: {
+    path: sysPath.join(root, "build"),
+    filename: "index.js"
+  },
+
+  resolve: {
+    root: srcRoot
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/i,
+        include: srcRoot,
+        loader: "babel"
+      }
+    ]
+  }
+};
