8162abb4e091dbcafd709797acd7f5e7344fb617
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2016-04-07 09:29:24 +0300

    Implement transactions system

--- src/createStore.js
+++ src/createStore.js
@@ -1,11 +1,16 @@
 import Bus from "kefir-bus";
-import { merge as merge$ } from "kefir";
+import { merge as $merge, constant as $constant, pool } from "kefir";
 import { defaultMutableStrategy } from "./storeUpdateStrategies";
 
 // ---
 
-const identity = x => x;
 const noop = () => {};
+const identity = x => x;
+const constant = x => () => x;
+
+const returnTrue = constant(true);
+const returnFalse$ = constant($constant(false));
+const conjAll = ar => ar.every(identity);
 
 // ---
 
@@ -15,7 +20,7 @@ function parseHandler(payloads$, val) {
 }
 
 function composeHandlersStream(actions$, handlersMap) {
-  return merge$(
+  return $merge(
     Object.keys(handlersMap).map(actionType => parseHandler(
       actions$.filter(({ type }) => type === actionType).map(({ payload }) => payload),
       handlersMap[actionType]
@@ -31,38 +36,84 @@ export default function createStore(
   initialState = {},
   strategy = defaultMutableStrategy
 ) {
-  const errors$ = new Bus();
   return (
-    composeHandlersStream(actions$.filter(({ type }) => type in handlers), handlers)
+    composeHandlersStream(
+      actions$.filter(({ type }) => type in handlers),
+      handlers
+    )
     .scan(
-      (state, { handler, payload }) => {
+      ({ state, error }, { handler, payload }) => {
         try {
-          return strategy(handler, state, payload);
-        } catch (e) {
-          // .scan should not return errors, because it will reset it's state
-          // @link https://github.com/rpominov/kefir/blob/c7870c6289d15a6037d8f0470229f03b04a23e56/src%2Fone-source%2Fscan.js#L22-L23
-          errors$.error(e);
-          return state;
+          return {
+            state: strategy(handler, state, payload),
+            error: null
+          };
+        } catch (error) {
+          return { state, error };
         }
       },
-      initialState
+      { state: initialState, error: null }
     )
-    .merge(errors$)
-    .toProperty() // store always has a current value
+    // same can be done via `.flatMap`, but why need to instantiate new stream on each event?
+    .withHandler((emitter, event) => {
+      if (event.type === "end") {
+        emitter.end();
+      } else {
+        const { state, error } = event.value;
+        error == null ? emitter.emit(state) : emitter.error(error);
+      }
+    })
     .onAny(noop) // activate stream immediately, so store will receive all dispatched actions
   );
 }
-
 
 export function createStoresFactory(
   actions$,
   defaultStrategy = defaultMutableStrategy
 ) {
+  const middleware = createTransactionsMiddleware(actions$);
+
   return function storesFactory(
     handlers,
     initialState,
     strategy = defaultStrategy
   ) {
-    return createStore(actions$, handlers, initialState, strategy);
+    return middleware(createStore(actions$, handlers, initialState, strategy));
+  };
+}
+
+// ---
+
+function createTransactionsMiddleware(actions$) {
+  let storesCount = 0;
+  const storesPool$ = pool();
+  const transactions$ = storesPool$
+    // wait for all stores
+    .bufferWithCount({ valueOf: () => storesCount }) // dynamic buffer size
+    // ensure all stores has successfully processed action
+    .map(conjAll)
+    .toProperty(returnTrue);
+
+  return function transactionsMiddleware(store$) {
+    ++storesCount;
+
+    storesPool$.plug(
+      store$
+        .map(returnTrue)
+        .flatMapErrors(returnFalse$)
+        .bufferBy(actions$) // TODO: is it possible to decouple this middleware from actions$ ?
+        .map(conjAll)
+    );
+
+    return store$
+      // flush whenever transaction completed (no matter which store produced it)
+      .bufferBy(transactions$)
+      // only emit new state if transaction was successful
+      .filterBy(transactions$)
+      // In array should be 0 or 1 values (i.e. store either processed current action or not).
+      // `.flatten` won't emit if array is empty
+      .flatten()
+      // ensure store still always has a current state (`.flatten` turns it into steam mode)
+      .toProperty();
   };
 }
--- test/error_handling.js
+++ test/error_handling.js
@@ -29,37 +29,78 @@ describe("error handling", () => {
     assert.isFalse(onError.called);
   });
 
+  describe("transactions", () => {
 
-  it("should keep previous state when particular handler throws", () => {
-    app = setup({
-      defaultStoreUpdateStrategy: function immutableStrategy(handler, state, payload) {
-        const newState = Object.create(state);
-        handler(newState, payload);
-        return newState;
-      }
+    function simpleImmutableStrategy(handler, state, payload) {
+      const newState = Object.create(state);
+      handler(newState, payload);
+      return newState;
+    }
+
+    beforeEach(() => {
+      app = setup({
+        defaultStoreUpdateStrategy: simpleImmutableStrategy
+      });
     });
 
-    const initialState = { foo: 0 };
+    describe("should keep previous state when particular handler throws", () => {
+      it("for single store", () => {
 
-    const store = app.createStore(
-      {
-        foo: (state, payload) => state.foo += payload,
-        bar(state) {
-          state.bar = true;
-          throw new Error("test error");
-        }
-      },
-      initialState
-    );
+        const store = app.createStore(
+          {
+            foo: (state, payload) => state.foo += payload,
+            bar(state) {
+              state.bar = true;
+              throw new Error("test error");
+            }
+          },
+          { foo: 0 }
+        );
 
-    const observer = sinon.spy();
-    store.onValue(observer);
+        const observer = sinon.spy();
+        store.onValue(observer);
 
-    app.dispatch("foo", 1);
-    app.dispatch("bar");
-    app.dispatch("foo", 2);
+        app.dispatch("foo", 1);
+        app.dispatch("bar");
+        app.dispatch("foo", 2);
 
-    const finalState = observer.lastCall.args[0];
-    assert.deepEqual(finalState, { foo: 3 });
+        assert.equal(observer.callCount, 3);
+        assert.deepEqual(observer.lastCall.args[0], { foo: 3 });
+      });
+
+      it("for multiple stores", () => {
+        const fooObserver = sinon.spy();
+        const barObserver = sinon.spy();
+
+        const storeFoo = app.createStore(
+          {
+            foo: (state, payload) => {
+              state.foo += payload;
+              if (payload > 1) {
+                throw new Error("test error");
+              }
+            }
+          },
+          { foo: 0 }
+        ).onValue(fooObserver);
+
+        const storeBar = app.createStore(
+          {
+            bar: (state, payload) => state.bar += payload,
+            foo: (state, payload) => state.foo_bar += payload
+          },
+          { bar: 0, foo_bar: 0 }
+        ).onValue(barObserver);
+
+        app.dispatch("foo", 1);
+        app.dispatch("bar", 2);
+        app.dispatch("foo", 3);
+
+        assert.equal(fooObserver.callCount, 2);
+        assert.equal(barObserver.callCount, 3);
+        assert.deepEqual(fooObserver.lastCall.args[0], { foo: 1 });
+        assert.deepEqual(barObserver.lastCall.args[0], { bar: 2, foo_bar: 1 });
+      });
+    });
   });
 });
