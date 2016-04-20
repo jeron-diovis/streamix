bf19dac5d609f7509be5367ec08bd94d3316c23f
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2016-04-20 10:46:16 +0300

    Fix "isStream" helper

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-import { Stream } from "kefir";
+import { Observable } from "kefir";
 
 export const noop = () => {};
 export const identity = x => x;
 export const constant = x => () => x;
-export const isStream = x => x instanceof Stream;
+export const isStream = x => x instanceof Observable;
